c0a8ddf05c71c6ed7146402d446007d2f8081d55
RybaByba <Pulovo4ka@ukr.net>
2020-11-13 10:18:21 +0200

    apdate_HW_1

--- Lesson1/Homework/HW_1.py
+++ Lesson1/Homework/HW_1.py
@@ -17,8 +17,12 @@ if ord(let1) and ord(let2) in range(97, 123):
     print(f"літера, \"{let2}\", має номер в ASCI коді ", ord(let2))
     print(f"літера, \"{let1}\", в англійському алфавіті має номер", ord(let1) - 96)
     print(f"літера, \"{let2}\", в англійському алфавіті має номер", ord(let2) - 96)
+
     if ord(let1) == ord(let2):
         print(f"між літерою, \"{let1}\", та літерою, \"{let2}\", не має відстані. Ви вказали одну і ту саму літеру.")
+    elif ord(let1) > ord(let2):
+        distance = abs((ord(let1) - ord(let2)) - 1)
+        print(f"між літерою, \"{let1}\", та літерою, \"{let2}\", відстань, {distance}, символи(ів)")
     else:
         distance = abs((ord(let1) - ord(let2)) + 1)
         print(f"між літерою, \"{let1}\", та літерою, \"{let2}\", відстань, {distance}, символи(ів)")
